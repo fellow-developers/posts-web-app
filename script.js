5125c950af1c6acc901337bd432460f9a4ba6dd9
Kunal Mishra <kunalmishra8127@gmail.com>
2024-09-30 23:42:43 +0530

    updated changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 async function fetchTodo() {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/todos');
@@ -5,20 +7,22 @@ async function fetchTodo() {
             throw new Error('Network response was not ok');
         }
         const json = await response.json();
-        json.forEach((item) => appendData(item.title));
+        appendData(json)
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
 }
 
-const appendData = (title) => {
-    const container = document.querySelector('.append-titles')
-    const newTitle = document.createElement('span')
-    newTitle.innerHTML =  title;
-    container.appendChild(newTitle)
+const appendData = (data) => {
+    data.forEach((item) => {
+        const container = document.querySelector('.append-titles')
+        const newTitle = document.createElement('span')
+        newTitle.innerHTML =  item.title;
+        container.appendChild(newTitle)
+    })
 }
 
 window.onload = () => {
     const button = document.getElementById('fetch-api-button');
-    button.addEventListener('click', () => fetchTodo())
+    button.addEventListener('click',  fetchTodo())
 }
