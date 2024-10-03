a8583119e669ba35a3baa4a0c6ebe6bbe1a9f9bc
Kunal Mishra <kunalmishra8127@gmail.com>
2024-10-03 20:14:54 +0530

    added a helper method to pass data to appendData function

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-async function fetchTodo() {
+
+const postApi = "https://jsonplaceholder.typicode.com/todos";
+
+// Fetch API Function
+async function fetchTodo(apiUrl) {
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const response = await fetch(apiUrl);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const json = await response.json();
-        return json
+        return json;
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
 }
 
- async function appendData() {
-    const data =  await fetchTodo();
-    await data.forEach((item) => {
-        const container = document.querySelector('.append-titles')
-        const newTitle = document.createElement('span')
-        newTitle.innerHTML =  item.title;
-        container.appendChild(newTitle)
-    })
+
+// Append data in DOM 
+async function appendData(data) {
+    const container = document.querySelector('.append-titles');
+    data.forEach((item) => {
+        const newTitle = document.createElement('span');
+        newTitle.innerHTML = item.title;
+        container.appendChild(newTitle);
+    });
+}
+
+// Helper function to store data from fetch function and pass on to appendData function
+const getData = async () => {
+    const data = await fetchTodo(postApi);
+    await appendData(data);
 }
 
 window.onload = () => {
     const button = document.getElementById('fetch-api-button');
-    button.addEventListener('click',  appendData)
+    button.addEventListener('click', getData);
 }
