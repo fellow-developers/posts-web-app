27408e12324edbaca4d4f94c9864fd49d79fc567
Kunal Mishra <kunalmishra8127@gmail.com>
2025-02-09 14:34:51 +0530

    done with gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass")); // Explicitly require the sass compiler
+
+gulp.task("sass", function (done) {
+  return gulp
+    .src("**/*.scss") // Your SCSS files
+    .pipe(sass().on("error", sass.logError)) // Compile Sass and log errors
+    .pipe(gulp.dest("dist/css")); // Output to CSS folder
+  done(); // Ensure async completion is signaled
+});
+
+gulp.task("default", gulp.series("sass"));
