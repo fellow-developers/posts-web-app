5ab7fba48907ba75cf191b68ec84a52a0bfdac8c
Kunal Mishra <kunalmishra8127@gmail.com>
2025-02-14 23:19:00 +0530

    resolved-conversations

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass")); // Explicitly require the sass compiler
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps"); // Add gulp-sourcemaps
 
-gulp.task("sass", function (done) {
+gulp.task("sass", function () {
   return gulp
-    .src("**/*.scss") // Your SCSS files
-    .pipe(sass().on("error", sass.logError)) // Compile Sass and log errors
-    .pipe(gulp.dest("dist/css")); // Output to CSS folder
+    .src("**/*.scss") // You can adjust the path as needed
+    .pipe(sourcemaps.init()) // Initialize sourcemaps before Sass compilation
+    .pipe(sass().on("error", sass.logError)) // Compile Sass to CSS
+    .pipe(cleanCSS({ compatibility: "ie8" })) // Minify CSS
+    .pipe(sourcemaps.write("./maps")) // Output sourcemaps to a 'maps' directory
+    .pipe(gulp.dest("dist/css")); // Destination for the compiled CSS
 });
-
-gulp.task("default", gulp.series("sass"));
