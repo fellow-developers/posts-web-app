eb6ed80e35d64a602b6f7c3d3004388ea983df7f
Kunal Mishra <kunalmishra8127@gmail.com>
2025-02-15 00:08:46 +0530

    remove clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const cleanCSS = require("gulp-clean-css");
 const sourcemaps = require("gulp-sourcemaps"); // Add gulp-sourcemaps
 
 gulp.task("sass", function () {
   return gulp
     .src("**/*.scss") // You can adjust the path as needed
     .pipe(sourcemaps.init()) // Initialize sourcemaps before Sass compilation
-    .pipe(sass().on("error", sass.logError)) // Compile Sass to CSS
-    .pipe(cleanCSS({ compatibility: "ie8" })) // Minify CSS
+    .pipe(sass({ style: "compressed" }).on("error", sass.logError)) // Compile Sass to CSS
     .pipe(sourcemaps.write("./maps")) // Output sourcemaps to a 'maps' directory
     .pipe(gulp.dest("dist/css")); // Destination for the compiled CSS
 });
